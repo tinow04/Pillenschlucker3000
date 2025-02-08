7a958b99a7b294cd8e25b2a086c973bdc2407465
tinow04 <tino.wolter2@gmail.com>
2025-02-08 23:59:06 +0100

    changed Structure

---
+++ Frontend/src/components/feature/game/src/pages/GamePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>
+      You’ve successfully created a project with Vue
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
