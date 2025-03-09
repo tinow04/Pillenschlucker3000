d49269c756a4a462a365a0686edd3ca6b82ac78e
Philipp5 <philipp.markl.9@gmail.com>
2025-03-09 19:41:56 +0100

    homepage and  loginpage

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,27 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import GamePage from "@/components/feature/game/src/pages/GamePage.vue";
-import HomePage from "@/components/feature/home/src/pages/HomePage.vue";
-import LoginPage from "@/components/feature/login/src/pages/LoginPage.vue";
+import GamePage from "../components/feature/game/src/pages/GamePage.vue";
+import HomePage from '../components/feature/home/src/pages/HomePage.vue';
+import LoginPage from '../components/feature/login/src/pages/LoginPage.vue';
+
+const routes = [
+  { path: '/', component: HomePage},
+  { path: '/login', component: LoginPage },
+  { path: '/game', component: GamePage },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginPage,
-    },
-    {
-      path: '/game',
-      name: 'game',
-      component: GamePage,
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
