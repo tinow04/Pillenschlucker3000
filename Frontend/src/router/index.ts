efcfe3e17cd4c223946d439bee64ae8aad156266
Jonah199 <j.haeuser4@gmail.com>
2025-06-24 18:33:39 +0200

    Coins added locker (#12)
    * Development Warnings fixed
    
    * Development Warnings fixed
    
    * Development warnings fixed
    
    * Development warnings fixed
    
    * Development warnings fixed
    
    * Development warnings fixed
    
    * Development warnings fixed

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -2,15 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import GamePage from "../components/feature/game/src/pages/GamePage.vue";
 import HomePage from '../components/feature/home/src/pages/HomePage.vue';
 import LoginPage from '../components/feature/login/src/pages/LoginPage.vue';
-import PacmanShopPage from '../components/feature/pacmanShop/pages/PacmanShopPage.vue';
-import LockerPacman from '../components/feature/newPacmanShop/pages/LockerPacman.vue';
+import Locker from '../components/feature/newPacmanShop/pages/Locker.vue'
 
 const routes = [
   { path: '/', component: HomePage},
   { path: '/login', component: LoginPage },
   { path: '/ingame', component: GamePage },
-  { path: '/pacmanShop', component: PacmanShopPage},
-  { path: '/locker', component: PacmanShopPage }
+  { path: '/locker', component: Locker }
 ]
 
 const router = createRouter({
