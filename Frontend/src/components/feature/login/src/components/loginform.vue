dfd61dd71382dd4b6c1d227bdc26932e3ce04025
Philipp5 <philipp.markl.9@gmail.com>
2025-06-26 22:48:04 +0200

    Changed Username to Email

--- Frontend/src/components/feature/login/src/components/loginform.vue
+++ Frontend/src/components/feature/login/src/components/loginform.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-const username = ref('')
+const email = ref('')
 const password = ref('')
 const errorMessage = ref('');
-
+const router = useRouter();
 const handleLoginSubmit = async () => {
   try {
     const response = await fetch('http://localhost/api/login', {
@@ -13,7 +14,7 @@ const handleLoginSubmit = async () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        username: username.value,
+        email: email.value,
         password: password.value,
       }),
     });
@@ -25,6 +26,7 @@ const handleLoginSubmit = async () => {
     const data = await response.json();
     console.log("Erfolg:", data);
     console.log(data.user.id)
+    router.push("/");
   } catch (error) {
     console.error('Fehler:', error);
     errorMessage.value = "Login fehlgeschlagen. Bitte versuchen Sie es erneut.";
