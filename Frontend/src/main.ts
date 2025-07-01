cb619cce21aba9a84316efae0a633b86817f042a
Tino <80458867+tinow04@users.noreply.github.com>
2025-07-01 18:07:25 +0200

    made Pinia persistent (#36)

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,10 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
