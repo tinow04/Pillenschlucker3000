d1c872e9bb6b0847e91a296e4603d7f82ebccdbe
phmarkl <philipp.markl@mercedes-benz.com>
2025-07-03 13:30:38 +0200

    Implemented Sounds

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import ToastManager from "@/components/devPanel/ToastManager.vue";
 import DevPanel from "@/components/devPanel/DevPanel.vue";
+import Sounds from "@/components/sounds/sounds.vue";
 </script>
 
 <template>
   <router-view />
   <ToastManager />
   <DevPanel />
+  <Sounds />
 </template>
 
 <style scoped></style>
