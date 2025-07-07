6f37907e6880dcb909832fe370f99b974a69de20
Philipp <philipp.markl.9@gmail.com>
2025-07-07 14:38:34 +0200

    Ingame res test (#64)
    * Test Branch for Ingame Res
    
    * Changed something in ingameRes
    
    * Finished Ingame Scaling

--- Frontend/src/components/feature/game/src/pages/GamePage.vue
+++ Frontend/src/components/feature/game/src/pages/GamePage.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
 import GameField from '../components/GameField.vue';
+
+function updateZoomDynamically() {
+  const BASE_WIDTH = 1600;
+  const BASE_HEIGHT = 800;
+
+  const scaleX = window.innerWidth / BASE_WIDTH;
+  const scaleY = window.innerHeight / BASE_HEIGHT;
+  const scale = Math.min(scaleX, scaleY, 1);
+
+  document.documentElement.style.zoom = scale.toFixed(3);
+}
+
+onMounted(() => {
+  updateZoomDynamically();
+  window.addEventListener('resize', updateZoomDynamically);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateZoomDynamically);
+  document.documentElement.style.zoom = '1.0';
+});
 </script>
 
 <template>
   <div class="page-container">
-    <GameField class="field-box"></GameField>
+    <GameField class="field-box" />
   </div>
 </template>
 
